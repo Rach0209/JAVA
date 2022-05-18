21667aca67bc73c6a00da1472251d8cb47bb6e15
Rach0209 <104557592+Rach0209@users.noreply.github.com>
2022-05-18 17:16:38 +0900

    Add files via upload

--- BSS/05_18/CircleVol.java
+++ BSS/05_18/CircleVol.java
@@ -8,10 +8,15 @@ public class CircleVol {
 		Scanner radius = new Scanner(System.in);
 		
 		System.out.print("반지름의 값을 입력하세요: ");
+		
+		
 		double r = radius.nextDouble();
 		double r3 = 4 * (r * r * r) / 3;
 		
-		System.out.print("구의 부피는 '" + r3 +"'입니다.");
+		double result = Math.pow(r, 3); // 함수식으로 계산하기 거듭제곱 계산 법. Math.power(double, double)
 		
+		
+		System.out.println("구의 부피는 '" + r3 +"'입니다.");
+		System.out.println("구의 부피는 '" + (4 * result / 3) +"'입니다.");
 	}
 }
