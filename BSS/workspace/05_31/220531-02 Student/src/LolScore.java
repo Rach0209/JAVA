88a84ce7f93d4294e933386051717fd39a6a4e82
Rach0209 <104557592+Rach0209@users.noreply.github.com>
2022-05-31 17:47:24 +0900

    Add files via upload

---
+++ BSS/workspace/05_31/220531-02 Student/src/LolScore.java
@@ -0,0 +1,15 @@
+// 학급(반)
+// 학생
+// 학생
+// 학생
+
+// 1. 3명의 학생의 이름을 콘솔에 출력할 수 있음.
+// 2. 학생 3명의 총 평균을 알려줄 수 있음.
+// 3. 평균점수가 가장 높은 학생(참조)을 알려줄 수 있음(return type 학생). => 동일 점수. 학생 수 1 < 2 < 3 순
+public class LolScore {
+	public static void main(String[] args) {
+		LolName print = new LolName();
+		print.printOrder1();
+		print.printOrder2();
+	}
+}
